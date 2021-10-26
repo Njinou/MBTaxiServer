91ac33494e23847d418d229b57fd9ae51f709cf3
Pascal Nitcheu <nitcheupascal@gmail.com>
2021-10-26 21:40:35 +0100

    saving server trigger rides and accept rides

--- functions/index.js
+++ functions/index.js
@@ -33,7 +33,7 @@ admin.initializeApp();
       if (!points) throw new Error("points is required");
       
       let finalArray = [];
-      let arraySize = points.features.length<=10? points.features.length: 10;
+      let arraySize = points.features.length<=20? points.features.length: 20;
       var neighborArray =  new Array(arraySize);
       for (j=0 ; j <neighborArray.length; j++){
 
@@ -87,7 +87,7 @@ admin.initializeApp();
 
 
     exports.driversOnClientDestination = functions.database.ref('users/destinationPoint/{destinationId}/clients/{values}/{pushID}')
-    .onWrite( (snapshot, context) => {
+    .onCreate( (snapshot, context) => {
       // Grab the current value of what was written to the Realtime Database.
      // const original = snapshot.val();
      // functions.logger.log('drivers on the path', context.params.destinationId, original);
@@ -117,13 +117,16 @@ admin.initializeApp();
             admin.database().ref('/users/destinationPoint/' +context.params.destinationId  + '/driverOnThePath').child(db).set(lam)
             
             const driversP = await admin.database().ref('/drivers/position/').child(lam.driverID).once('value')// orderByKey().equalTo(lam.driverID).once('value')
-            
             driverPosition = driversP.val()
             driverPosition = driverPosition.split(",")
             driverPosition = driverPosition.map( elmnt =>  Number((elmnt.replace("+","."))))
+            
+            const placeAvail = await admin.database().ref('/drivers/availablePl/').child(lam.driverID).once('value')// orderByKey().equalTo(lam.driverID).once('value')
+            let place  = placeAvail.val()
             let obj= {};
             obj.driverId = lam.driverID;
             obj.point = driverPosition;
+            obj.place = place;
             return obj;
         })
       ) 
@@ -134,21 +137,50 @@ admin.initializeApp();
       let voisinageClientDriverID = turf.featureCollection( AllFavouriteDriversPosition.map (elmnt => {
           let  obja= turf.point(elmnt.point)
           obja.driverID = elmnt.driverId
+          obja.place = elmnt.place
          return obja;
        }))
        
        //choisir les 10 plus proches ... chauffeurs allant dans cette directions.... 
        let chauffeurFavorable = neighborPointMod(voisinageClient,voisinageClientDriverID);
+       let pickupPointVal = context.params.values;
 
-       admin.database().ref('/users/favori/').child(context.params.values).set(chauffeurFavorable);
-        return snapshot.ref.parent.parent.child('DriverInDestinationDirection').set({tester:"voir..."});
+       admin.database().ref('/users/favoTaxis/'+ context.params.destinationId).child (pickupPointVal).set(chauffeurFavorable);
+     //  admin.database().ref('/users/destinationPoint').child(context.params.destinationId).set({});
+      //  return snapshot.ref.parent.parent.child('DriverInDestinationDirection').set({tester:"voir..."});
         
      })
 
-     
+ //    return snapshot.ref.parent.parent.child('resultat/Proximite').set({tester:"voir... ce que je te dis depuis trop de familiarite engendre enormement de mepris"})
     });
 
 
+exports.filterDriver = functions.database.ref('/requests/{destinationID}/{departureID}/{nbreOfPeople}/{uid}/')
+.onCreate( async (snapshot, context) => {
+  let clientID =  snapshot.val();
+  let destination = context.params.destinationID;
+  let departure = context.params.departureID;
+  let nbreOfPeople = context.params.nbreOfPeople;
+  let uid = context.params.uid;
+
+  const favoritesDrivers = await admin.database().ref('/users/favoTaxis/' + destination).child(departure).once('value')// orderByKey().equalTo(lam.driverID).once('value')
+  //functions.logger.log('favoritesDrivers favoritesDrivers favoritesDrivers favoritesDrivers favoritesDrivers favoritesDrivers',favoritesDrivers.val());
+
+  let Drivers  = favoritesDrivers.val()
+  let filteredDrivers = Drivers.map( driv => {
+    if (driv.place >= parseInt(nbreOfPeople)) {
+      let obj={};
+      obj.destination = destination;
+      obj.departure = departure;
+      obj.driverID = driv.driverID;
+      obj.nbreOfPeople = nbreOfPeople;
+      obj.clientID = clientID;
+      admin.database().ref('/drivers/alerting').child(driv.driverID).push(obj);
+      return driv;
+    }
+  }).filter(Boolean);
+  admin.database().ref('/users/potentialMatch/' + destination + '/' + departure).child(clientID).set(filteredDrivers);
+ });
 
 
 
